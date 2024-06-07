2cdbbd7829721c1d8c089a021aa7996268302d04
Shafy <shafyali433@gmail.com>
2024-06-07 12:06:04 +0500

    Add Production Url

--- utills/index.ts
+++ utills/index.ts
@@ -1,4 +1,4 @@
 
 export const getDomain = () => {
-  return new URL(process.env.NODE_ENV === 'production' ? "https://discover-coffee-store.vercel.com":"http://localhost:3000")
+  return new URL(process.env.NODE_ENV === 'production' ? "https://coffee-store-nextjs-ktg9.vercel.app/":"http://localhost:3000")
 }
